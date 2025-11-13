3b3804413626d1374d8aa2b45c7384f002078424
mullerg <gabriel_muller98@hotmail.com>
2025-11-13 09:42:31 -0300

    chore: env config module

--- src/shared/infraestructure/env-config/env-config.module.ts
+++ src/shared/infraestructure/env-config/env-config.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EnvConfigService } from './env-config.service';
+import { join } from 'node:path';
 
 @Module({
-  providers: [EnvConfigService]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        join(__dirname, `../../../../.env.${process.env.NODE_ENV ?? 'development'}`),
+      ],
+    }),
+  ],
+  providers: [EnvConfigService],
 })
 export class EnvConfigModule {}
--- src/shared/infraestructure/env-config/env-config.service.ts
+++ src/shared/infraestructure/env-config/env-config.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private configService: ConfigService) {}
+
+  getAppPort(): number {
+    return Number(this.configService.get('PORT'));
+  }
+
+  getNodeEnv(): string {
+    const nodeEnv = this.configService.get<string>('NODE_ENV');
+    if (!nodeEnv) {
+      throw new Error('NODE_ENV is not defined in environment variables.');
+    }
+    return nodeEnv;
+  }
+}
